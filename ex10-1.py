0c95db27349cc9fcc6b943940f54c8ce876debbf
Jaehee <jaehee0395@gmail.com>
2022-12-29 13:33:01 +0900

    update gitignore

--- ex10-1.py
+++ ex10-1.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import os
 import string
+import numpy as np
+import glob
+from torch.utils.data.dataset import Dataset
 
-df = pd.read_csv()
+df = pd.read_csv("data/CH10/*.csv")
+
+class TextGeneration(Dataset):
+    def clean_test(self, txt):
+        txt ="".join(v for v in txt if v not in string.punctuation).lower()
+        return txt
+
